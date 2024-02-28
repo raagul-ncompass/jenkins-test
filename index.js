db2c8df354ec741ee85f655aac1b3b95f536a636
raagul-ncompass <raagul@ncompass.inc>
2024-02-28 10:06:06 +0530

    init

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hello");
+process.stdout.write("hello ");
+if(process.argv[2]){
+    console.log(process.argv[2])
+}
--- index.py
+++
@@ -1,6 +0,0 @@
-import sys
-print("hello", end=" ")
-if len(sys.argv) > 1:
-    print(sys.argv[1])
-
-
